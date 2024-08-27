d693e231f3b5328a3858c4354b16faecc186246a
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-08-27 07:48:00 +0700

    Update omni_spm8667p1_64.mk

--- omni_spm8667p1_64.mk
+++ omni_spm8667p1_64.mk
@@ -11,6 +11,11 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+
 # Inherit from spm8667p1_64 device
 $(call inherit-product, device/spm8667p1_64/device.mk)
 
